0a8964b0bc07d1edef2dd0ebdefba35af08eec4b
Drew <57231604+dschaly2@users.noreply.github.com>
2023-10-26 19:45:25 -0400

    returns distance and all nodes in path

--- dijkstra.py
+++ dijkstra.py
@@ -3,6 +3,7 @@ import numpy as np
 def dijkstra(adj_matrix, start, end):
     num_nodes = len(adj_matrix)
     distances = [float('inf')] * num_nodes
+    predecessors = [None] * num_nodes
     distances[start] = 0
 
     unvisited = list(range(num_nodes))
@@ -12,7 +13,7 @@ def dijkstra(adj_matrix, start, end):
         unvisited.remove(current_node)
 
         if current_node == end:
-            return distances[end]  # Return the distance when the end node is reached
+            return distances[end], reconstruct_path(predecessors, start, end)  # Return distance and path
 
         for neighbor in range(num_nodes):
             if adj_matrix[current_node][neighbor] > 0:
@@ -20,24 +21,29 @@ def dijkstra(adj_matrix, start, end):
 
                 if distance < distances[neighbor]:
                     distances[neighbor] = distance
+                    predecessors[neighbor] = current_node
 
-    return float('inf')  # Return infinity if no path to the end node is found
+    return float('inf'), []  # Return infinity and an empty path if no path to the end node is found
 
-# Example usage:
+def reconstruct_path(predecessors, start, end):
+    path = []
+    while end is not None:
+        path.insert(0, end)
+        end = predecessors[end]
+    return path
+
+# Input Adjacency Matrix
 adjacency_matrix = np.array([
-    [0, 4, 8, 6, 12],
-    [4, 0, 6, 15, 3],
-    [8, 6, 0, 14, 7],
-    [12, 3, 7, 8, 0]
+    # Matrix Here
 ])
 
 start_node = 0
 end_node = 3
 
-shortest_distance = dijkstra(adjacency_matrix, start_node, end_node)
+shortest_distance, shortest_path = dijkstra(adjacency_matrix, start_node, end_node)
 
 if shortest_distance < float('inf'):
     print(f"Shortest distance from {start_node} to {end_node}: {shortest_distance}")
+    print(f"Shortest path: {shortest_path}")
 else:
     print(f"No path from {start_node} to {end_node}")
-
