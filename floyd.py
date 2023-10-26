6452d44441a5e348edd8c25b80c9bf6c8ae05609
Drew <57231604+dschaly2@users.noreply.github.com>
2023-10-26 19:47:18 -0400

    returns nodes in path now

--- floyd.py
+++ floyd.py
@@ -1,33 +1,41 @@
 import numpy as np
 
-def floyd_warshall(adj_matrix):
+def floyd_warshall_with_path(adj_matrix):
     num_nodes = len(adj_matrix)
     distances = np.copy(adj_matrix)
+    predecessors = np.full((num_nodes, num_nodes), -1, dtype=int)
 
     for k in range(num_nodes):
         for i in range(num_nodes):
             for j in range(num_nodes):
                 if distances[i][j] > distances[i][k] + distances[k][j]:
                     distances[i][j] = distances[i][k] + distances[k][j]
+                    predecessors[i][j] = k  # Store the intermediate node k
 
-    return distances
+    return distances, predecessors
 
-# Example usage:
+def reconstruct_path(predecessors, start, end):
+    if predecessors[start][end] == -1:
+        return [start, end]
+    intermediate = predecessors[start][end]
+    path1 = reconstruct_path(predecessors, start, intermediate)
+    path2 = reconstruct_path(predecessors, intermediate, end)
+    return path1[:-1] + path2
+
+
 adjacency_matrix = np.array([
-    [0, 4, 8, 6, 12],
-    [4, 0, 6, 15, 3],
-    [8, 6, 0, 14, 7],
-    [12, 3, 7, 8, 0]
+    # Matrix here
 ])
 
-start_node = 0
-end_node = 3
+start_node = 6
+end_node = 17
 
-all_shortest_distances = floyd_warshall(adjacency_matrix)
+all_shortest_distances, predecessors = floyd_warshall_with_path(adjacency_matrix)
 shortest_distance = all_shortest_distances[start_node][end_node]
 
 if shortest_distance < float('inf'):
     print(f"Shortest distance from {start_node} to {end_node}: {shortest_distance}")
+    shortest_path = reconstruct_path(predecessors, start_node, end_node)
+    print(f"Shortest path: {shortest_path}")
 else:
     print(f"No path from {start_node} to {end_node}")
-
