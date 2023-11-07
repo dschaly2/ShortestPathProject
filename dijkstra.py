546ec6c4bceef651b9b8d33d91647830f50cfb00
ibrahimsabieh <45676955+ibrahimsabieh@users.noreply.github.com>
2023-11-06 22:42:06 -0500

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -5,9 +5,10 @@ from config import dataset
 import pandas as pd
 from tabulate import tabulate
 
+
 def dijkstra(adj_matrix, start, end):
     num_nodes = len(adj_matrix)
-    distances = [float('inf')] * num_nodes
+    distances = [float("inf")] * num_nodes
     predecessors = [None] * num_nodes
     distances[start] = 0
 
@@ -18,7 +19,9 @@ def dijkstra(adj_matrix, start, end):
         unvisited.remove(current_node)
 
         if current_node == end:
-            return distances[end], reconstruct_path(predecessors, start, end)  # Return distance and path
+            return distances[end], reconstruct_path(
+                predecessors, start, end
+            )  # Return distance and path
 
         for neighbor in range(num_nodes):
             if adj_matrix[current_node][neighbor] > 0:
@@ -28,7 +31,11 @@ def dijkstra(adj_matrix, start, end):
                     distances[neighbor] = distance
                     predecessors[neighbor] = current_node
 
-    return float('inf'), []  # Return infinity and an empty path if no path to the end node is found
+    return (
+        float("inf"),
+        [],
+    )  # Return infinity and an empty path if no path to the end node is found
+
 
 def reconstruct_path(predecessors, start, end):
     path = []
@@ -37,34 +44,38 @@ def reconstruct_path(predecessors, start, end):
         end = predecessors[end]
     return path
 
-def run(testcase, start, end):
 
+def run(testcase, start, end):
     results = []
 
     time_start = time.process_time()
-    
+
     shortest_distance, shortest_path = dijkstra(dataset(testcase), start, end)
 
-    if shortest_distance < float('inf'):
+    if shortest_distance < float("inf"):
         results.append(f"{start} to {end}: {shortest_distance}")
         results.append(f"{shortest_path}")
 
-        time_elapsed = ((time.process_time() - time_start))
-        memMb=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024.0/1024.0
+        time_elapsed = time.process_time() - time_start
+        memMb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0 / 1024.0
         results.append("{:.5f} secs {:.5f} MByte".format(time_elapsed, memMb))
-        df = pd.DataFrame(results, index=['Shortest Distance', 'Nodes Traveled', 'Time/Memory'], columns=['Data'])
+        df = pd.DataFrame(
+            results,
+            index=["Shortest Distance", "Nodes Traveled", "Time/Memory"],
+            columns=["Data"],
+        )
     else:
         print(f"No path from {start} to {end}")
 
-        time_elapsed = ((time.process_time() - time_start))
-        memMb=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024.0/1024.0
+        time_elapsed = time.process_time() - time_start
+        memMb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0 / 1024.0
         results.append("{:.5f} secs {:.5f} MByte".format(time_elapsed, memMb))
 
-    return tabulate(df, headers = 'keys', tablefmt = 'psql')
+    return tabulate(df, headers="keys", tablefmt="psql")
 
 
-print(run("Bestcase", 0, 9))
-print(run("Worstcase", 0, 5))
-print(run("Smallest", 0, 3))
-print(run("Longest", 7, 21))
-print(run("Blockedcase", 3,8))
+print(run("Bestcase", 4, 17))
+print(run("Worstcase", 6, 24))
+print(run("Smallest", 2, 4))
+print(run("Longest", 23, 88))
+print(run("Blockedcase", 0, 24))
